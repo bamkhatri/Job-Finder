749fb645f8062126846b2206d84af4fd3a4a92a9
bamkhatri <bamkhatri24@gmail.com>
2022-08-30 15:08:30 +0545

    Error page completed

---
+++ client/src/pages/Error.js
@@ -0,0 +1,16 @@
+import img from '../assets/images/not-found.svg'
+import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/ErrorPage'
+const Error = () => {
+  return (
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='Not Found' />
+        <h3> Ohh! Page Not found</h3>
+        <p>We can't seem to found the page you're looking for</p>
+        <Link to='/'>Back Home</Link>
+      </div>
+    </Wrapper>
+  )
+}
+export default Error
